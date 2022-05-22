f37a2d55f0dcb621ca9099d5289ea27398acc5ee
johnsey <johnsey.reynolds@gmail.com>
2022-05-22 06:56:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 /* Your Code Here */
+const createEmployeeRecord = function (row) {
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+const createEmployeeRecords = function (employeeRowData) {
+    return employeeRowData.map(function (row) {
+        return createEmployeeRecord(row)
+    })
+}
+const createTimeInEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+const createTimeOutEvent = function (dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+const hoursWorkedOnDate = function (seekDate) {
+    const inEvent = this.timeInEvents.find(function (e) {
+        return e.date === seekDate
+    })
+
+    const outEvent = this.timeOutEvents.find(function (e) {
+        return e.date === seekDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+const wagesEarnedOnDate = function (dateSeek) {
+    const rawWage = hoursWorkedOnDate.call(this, dateSeek)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +74,16 @@ const allWagesFor = function () {
 
     return payable
 }
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+const calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+
 
